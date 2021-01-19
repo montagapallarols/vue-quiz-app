9d34bd3f234d00fe151c11e00e57dc394e6c3e02
montipallarols <montaga.pallarols@gmail.com>
2021-01-19 14:44:12 +0100

    Fetch data from api, save in questions array

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <Header />
-    <QuestionBox />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -14,6 +20,22 @@ export default {
   components: {
     Header,
     QuestionBox,
+  },
+  data() {
+    return {
+      questions: [],
+    };
+  },
+  mounted: function() {
+    fetch("https://opentdb.com/api.php?amount=10&category=9", {
+      method: "get",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results;
+      });
   },
 };
 </script>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,24 +1,18 @@
 <template>
   <div>
-    <b-container class="bv-example-row">
-      <b-row>
-        <b-col sm="6" offset="3">
-          <b-jumbotron>
-            <template #lead>
-              Here goes the question?
-            </template>
+    <b-jumbotron>
+      <template #lead>
+        Here goes the question?
+      </template>
 
-            <hr class="my-4" />
+      <hr class="my-4" />
 
-            <p>
-              List of answers
-            </p>
+      <p>
+        List of answers
+      </p>
 
-            <b-button variant="primary" href="#">Submit</b-button>
-            <b-button variant="success" href="#">Next</b-button>
-          </b-jumbotron>
-        </b-col>
-      </b-row>
-    </b-container>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
   </div>
 </template>
