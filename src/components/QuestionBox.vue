0e7fd76fc2a8ffc2c11b85865410b79836f59285
montipallarols <montaga.pallarols@gmail.com>
2021-01-19 16:04:46 +0100

    Get index of selected answer and save it in data

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -9,9 +9,12 @@
       <hr class="my-4" />
 
       <b-list-group>
-        <b-list-group-item v-for="(answer, index) in answers" :key="index">{{
-          answer
-        }}</b-list-group-item>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+          >{{ answer }}</b-list-group-item
+        >
       </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
@@ -27,12 +30,25 @@ export default {
     currentQuestion: Object,
     next: Function,
   },
+  // Save index of selected answer
+  data() {
+    return {
+      selectedIndex: null,
+    };
+  },
   computed: {
     answers() {
       // Use 'this' to access a data prop
       let answers = [...this.currentQuestion.incorrect_answers];
       answers.push(this.currentQuestion.correct_answer);
       return answers;
+    },
+  },
+  methods: {
+    // On click function to get index of selected answer and save it in data
+    selectAnswer(index) {
+      this.selectedIndex = index;
+      // console.log(index);
     },
   },
 };
