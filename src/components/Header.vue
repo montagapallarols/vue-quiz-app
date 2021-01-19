8350b5501753a8a974da252c60c741d0300fd6b1
montipallarols <montaga.pallarols@gmail.com>
2021-01-19 14:09:30 +0100

    Create question box and center it

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <h1>Header</h1>
+    <b-nav>
+      <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+      <b-nav-item disabled>Counter: 4/10</b-nav-item>
+    </b-nav>
   </div>
 </template>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Question Box</h1>
+  <div>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <b-jumbotron>
+            <template #lead>
+              Here goes the question?
+            </template>
+
+            <hr class="my-4" />
+
+            <p>
+              List of answers
+            </p>
+
+            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button variant="success" href="#">Next</b-button>
+          </b-jumbotron>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
